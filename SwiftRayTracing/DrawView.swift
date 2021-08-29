a5265d3799df9faa6e768f5fd793fab0e8da8de3
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-08-29 18:01:08 +0900

    add draw view

---
+++ SwiftRayTracing/DrawView.swift
@@ -0,0 +1,24 @@
+//
+//  DrawView.swift
+//  SwiftRayTracing
+//
+//  Created by TokyoYoshida on 2021/08/29.
+//
+
+import UIKit
+
+class DrawView: UIView {
+    override func draw(_ rect: CGRect) {
+        drawFillCircle(rect: CGRect(x: 100, y: 100, width: 1000, height: 1000))
+    }
+
+    private func drawFillCircle(rect: CGRect) {
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return
+        }
+
+        context.setFillColor(UIColor.red.cgColor)
+
+        context.fillEllipse(in: rect)
+    }
+}
