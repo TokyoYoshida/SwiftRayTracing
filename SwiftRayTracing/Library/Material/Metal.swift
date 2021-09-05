cbe3b86ae50b983bbc1ac51848708d4ba95c28a1
tokyoyoshida <yoshidaforpublic@gmail.com>
2021-09-05 14:14:43 +0900

    add fuzz

--- SwiftRayTracing/Library/Material/Metal.swift
+++ SwiftRayTracing/Library/Material/Metal.swift
@@ -9,16 +9,17 @@ import Foundation
 
 class Metal: Material {
     var albedo: Color
+    var fuzz: Double
 
-    internal init(albedo: Color) {
+    internal init(albedo: Color, fuzz: Double) {
         self.albedo = albedo
+        self.fuzz = fuzz < 1 ? fuzz : 1
     }
 
     func scatter(rIn: Ray, rec: HitRecord, attenuation: inout Color, scatterd: inout Ray) -> Bool {
         let refrected = refrect(v: unitVector(v: rIn.direction), n: rec.normal)
-        scatterd = Ray(orig: rec.p, dir: refrected)
+        scatterd = Ray(orig: rec.p, dir: refrected + fuzz * randomInUnitSphere())
         attenuation = albedo
         return dot(scatterd.direction, rec.normal) > 0
     }
-
 }
